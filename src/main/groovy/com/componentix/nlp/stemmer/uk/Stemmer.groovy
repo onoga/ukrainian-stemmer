9852fe9eb0924047da2257d36835e4adc20c3994
Oleg Noga <o.noha@modnakasta.ua>
2016-06-03 16:25:16 +0300

    updated to compile static, elastic sec. manager did not like @Field

--- src/main/groovy/com/componentix/nlp/stemmer/uk/Stemmer.groovy
+++ src/main/groovy/com/componentix/nlp/stemmer/uk/Stemmer.groovy
@@ -1,118 +1,69 @@
 package com.componentix.nlp.stemmer.uk
 
-import groovy.transform.Field
+import groovy.transform.CompileStatic
 
-@Field def word_ends = """
-  а ам ами ах та
-  в вав вавсь вався вала валась валася вали вались валися вало валось валося вати ватись ватися всь вся
-  е еві ем ею
-  є ємо ємось ємося ється єте єтесь єтеся єш єшся єю
-  и ив ий ила или ило илося им ими имо имось имося ите итесь итеся ити ить иться их иш ишся
-  й ймо ймось ймося йсь йся йте йтесь йтеся
-  і ів ій ім імо ість істю іть
-  ї
-  ла лась лася ло лось лося ли лись лися
-  о ові овував овувала овувати ого ої ок ом ому осте ості очка очкам очками очках очки очків очкові очком очку очок ою
-  ти тись тися
-  у ував увала увати
-  ь
-  ці
-  ю юст юсь юся ють ються
-  я ям ями ях
-""".trim().split(/\s+/)
+@CompileStatic
+class Stemmer {
 
-// WAT ?
-// к ка кам ками ках ки кою ку
-// ні ню ня ням нями нях
-@Field def wends = word_ends.sort {-it.length()}
+    private static String[] word_ends = """
+      а ам ами ах та
+      в вав вавсь вався вала валась валася вали вались валися вало валось валося вати ватись ватися всь вся
+      е еві ем ею
+      є ємо ємось ємося ється єте єтесь єтеся єш єшся єю
+      и ив ий ила или ило илося им ими имо имось имося ите итесь итеся ити ить иться их иш ишся
+      й ймо ймось ймося йсь йся йте йтесь йтеся
+      і ів ій ім імо ість істю іть
+      ї
+      ла лась лася ло лось лося ли лись лися
+      о ові овував овувала овувати ого ої ок ом ому осте ості очка очкам очками очках очки очків очкові очком очку очок ою
+      ти тись тися
+      у ував увала увати
+      ь
+      ці
+      ю юст юсь юся ють ються
+      я ям ями ях
+    """.trim().split(/\s+/)
 
-// endings in unchangable words
+    // WAT ?
+    // к ка кам ками ках ки кою ку
+    // ні ню ня ням нями нях
+    private static String[] wends = word_ends.sort {-it.length()}
 
-@Field def stable_endings = """
-  ер
-  ск
-""".trim().split(/\s+/)
+    // endings in unchangable words
 
-@Field def skip_ends = stable_endings.sort {-it.length()}
+    private static String[] stable_endings = """
+      ер
+      ск
+    """.trim().split(/\s+/)
 
-// endings are changing
-@Field def change_endings = [
-  "аче" : "ак",
-  "іче" : "ік",
-  "йовував" : "йов", "йовувала" : "йов", "йовувати" : "йов",
-  "ьовував" : "ьов", "ьовувала" : "ьов", "ьовувати" : "ьов",
-  "цьовував" : "ц", "цьовувала" : "ц", "цьовувати" : "ц",
-  "ядер" : "ядр"
-]
+    private static String[] skip_ends = stable_endings.sort {-it.length()}
 
-// words to skip
-@Field def stable_exclusions = """
-  баядер беатріче
-  віче
-  наче неначе
-  одначе
-  паче
-""".trim().split(/\s+/)
+    // endings are changing
+    private static HashMap change_endings = [
+      "аче" : "ак",
+      "іче" : "ік",
+      "йовував" : "йов", "йовувала" : "йов", "йовувати" : "йов",
+      "ьовував" : "ьов", "ьовувала" : "ьов", "ьовувати" : "ьов",
+      "цьовував" : "ц", "цьовувала" : "ц", "цьовувати" : "ц",
+      "ядер" : "ядр"
+    ]
 
-// words to replace
-@Field def exclusions = [
-  "відер" : "відр",
-  "був" : "бува"
-]
+    // words to skip
+    private static String[] stable_exclusions = """
+      баядер беатріче
+      віче
+      наче неначе
+      одначе
+      паче
+    """.trim().split(/\s+/)
 
+    // words to replace
+    private static HashMap exclusions = [
+      "відер" : "відр",
+      "був" : "бува"
+    ]
 
-String stem(String word) {
-    // normalize word
-    word = replaceStressedVowels(word);
-
-    // don't change short words
-    if (word.length() <= 2 ) return word;
-
-    // check for unchanged exclusions
-    if (stable_exclusions.contains(word)) {
-        return word;
-    }
-
-    // check for replace exclusions
-    if (exclusions[word]) {
-        return exclusions[word];
-    }
-
-    // changing endings
-    // TODO order endings by abc DESC
-    for (eow in change_endings.keySet().sort { change_endings[it] }) {
-        if (word.endsWith(eow)) {
-            return word.substring(0, word.length() - eow.length()) + change_endings[eow]
-        }
-    }
-
-    // match for stable endings
-    for (eow in skip_ends) {
-        if (word.endsWith(eow)) {
-            return word
-        }
-    }
-
-    // try simple trim
-    for (eow in wends) {
-        if (word.endsWith(eow)) {
-            def trimmed = word.substring(0, word.length() - eow.length())
-            if (trimmed.length() > 2) {
-                return trimmed
-            }
-        }
-    }
-
-    return word
-}
-
-
-
-/*
- * Replace Ukrainian stressed vowels to unstressed ones
- */
-def replaceStressedVowels(word) {
-    def nagolos = [
+    private static HashMap nagolos = [
         "а́" : "а",
         "е́" : "е",
         "є́" : "є",
@@ -125,5 +76,50 @@ def replaceStressedVowels(word) {
         "я́" : "я"
     ]
 
-    return word.toLowerCase().collect { nagolos[it] ?: it }.join("")
+    @CompileStatic
+    public static String stem(String word) {
+        //Replace Ukrainian stressed vowels to unstressed ones
+        word = word.toLowerCase().collect { nagolos[it] ?: it }.join("")
+
+        // don't change short words
+        if (word.length() <= 2 ) return word;
+
+        // check for unchanged exclusions
+        if (stable_exclusions.contains(word)) {
+            return word;
+        }
+
+        // check for replace exclusions
+        if (exclusions[word]) {
+            return exclusions[word];
+        }
+
+        // changing endings
+        // TODO order endings by abc DESC
+        for (String eow in change_endings.keySet().sort { change_endings[it] }) {
+            if (word.endsWith(eow)) {
+                return word.substring(0, word.length() - eow.length()) + change_endings[eow]
+            }
+        }
+
+        // match for stable endings
+        for (String eow in skip_ends) {
+            if (word.endsWith(eow)) {
+                return word
+            }
+        }
+
+        // try simple trim
+        for (String eow in wends) {
+            if (word.endsWith(eow)) {
+                String trimmed = word.substring(0, word.length() - eow.length())
+                if (trimmed.length() > 2) {
+                    return trimmed
+                }
+            }
+        }
+
+        return word
+    }
+
 }
